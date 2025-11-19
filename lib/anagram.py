9325a4db306c04953a235479aa03bd666f637e62
George <gnm7208@gmail.com>
2025-11-19 19:32:58 +0300

    implement Anagram class with match method and add initial tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagram_list):
+        matches = []
+        for word in anagram_list:
+            if sorted(self.word) == sorted(word):
+                matches.append(word)
+        return matches
+    
+anagram = Anagram('listen')
+anagram_list = ['silent', 'enlists', 'google', 'inlets', 'banana']
+matches = anagram.match(anagram_list)
+print(matches)
+
+anagram = Anagram('acts')
+anagram_list = ['cats', 'tacos', 'act', 'acts', 'cast']
+matches = anagram.match(anagram_list)
+print(matches)
